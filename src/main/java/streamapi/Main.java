1bf75254d4630ca3dc6d14cf77d58440462e0311
thekodu <praveenrao116@gmail.com>
2024-11-24 07:28:25 +0530

    stream api

---
+++ src/main/java/streamapi/Main.java
@@ -0,0 +1,16 @@
+package streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = Arrays.asList(new Person(123, "first",24),
+                new Person(124,"second",25), new Person(125,"third",26));
+//        Stream.of(persons);
+
+//        for-each
+
+    }
+}
